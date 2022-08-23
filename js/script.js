f65a9ff871921fb73d3bab234360d1a03a4c1e37
Asm Nahin <asmnahin21@gmail.com>
2022-08-23 11:00:24 +0600

    script update

--- js/script.js
+++ js/script.js
@@ -1,37 +1,16 @@
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-container');
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
-let loginBtn = document.querySelector('#login-btn');
-let loginForm = document.querySelector('.login-form-container');
-let formClose = document.querySelector('#form-close');
 
 window.onscroll = () => {
    menu.classList.remove('fa-times');
    navbar.classList.remove('active');
-   searchBtn.classList.remove('fa-times');
-   searchBar.classList.remove('active');
+
 };
-
-searchBtn.addEventListener('click', () => {
-   searchBtn.classList.toggle('fa-times');
-   searchBar.classList.toggle('active');
-});
-
-loginBtn.addEventListener('click', () => {
-   loginForm.classList.add('active');
-});
-
-formClose.addEventListener('click', () => {
-   loginForm.classList.remove('active');
-});
 
 menu.onclick = () => {
    menu.classList.toggle('fa-times');
    navbar.classList.toggle('active');
 };
-
-
 
 var swiper = new Swiper(".home-slider", {
 
